8227625e8ddd25e7f686900bcc15bec057a7b3be
John Cuviello <jcuviell@uci.edu>
2023-04-30 00:04:13 -0700

    added logging statements on login

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -4,7 +4,9 @@ const baseUrl = '/api/login'
 let token = null
 
 const login = async credentials => {
+  console.log('in login')
   const res = await axios.post(baseUrl, credentials)
+  console.log('login res', res)
   return res.data
 }
 
