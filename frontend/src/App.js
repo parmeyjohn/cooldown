1f02aef7d04fdf5749967f8db105f893c54ce79e
John Cuviello <jcuviell@uci.edu>
2023-04-30 19:51:14 -0700

    switched from browserrouter to hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { EntryContext } from "./contexts/EntryContext";
 import { JournalContext } from "./contexts/JournalContext";
