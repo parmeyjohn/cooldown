53f8e2c9a1dae6dfa63c52ee8bc15d188bdab085
Nick Cuviello <nickcuv@gmail.com>
2023-11-02 01:53:47 -0700

    setup landing page component + prettier for tailwind

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,7 @@
+const Landing = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-y-auto bg-gradient-to-b from-teal-900 to-slate-800 text-teal-900"></div>
+  );
+};
+
+export default Landing;
