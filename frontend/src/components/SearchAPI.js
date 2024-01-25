8df4f24c1ab1f8ddfe6040c88ccac5b7513f46ea
John Cuviello <jcuviell@uci.edu>
2024-01-24 19:18:28 -0800

    added selection buttons for genre on create entry page

--- frontend/src/components/SearchAPI.js
+++ frontend/src/components/SearchAPI.js
@@ -5,6 +5,12 @@ import { Transition } from "@headlessui/react";
 import gamesService from "../services/games";
 
 import { ReactComponent as SearchIcon } from "../assets/heroicons/search.svg";
+import { ReactComponent as BookIcon } from "../assets/heroicons/book.svg";
+import { ReactComponent as MovieIcon } from "../assets/heroicons/movie.svg";
+import { ReactComponent as MusicIcon } from "../assets/heroicons/music.svg";
+
+import { GrGamepad as ControllerIcon } from "react-icons/gr";
+import GenreButton from "./GenreButton";
 
 const SearchAPI = ({
   searchValue,
@@ -14,6 +20,9 @@ const SearchAPI = ({
   placeholder,
 }) => {
   const [games, setGames] = useState([]);
+
+  const [genre, setGenre] = useState("Game");
+
   const [showGames, setShowGames] = useState(false);
   const [, cancel] = useDebounce(
     async () => {
@@ -42,9 +51,41 @@ const SearchAPI = ({
 
   return (
     <div className="flex w-full flex-col focus-within:text-gray-700 ">
+      <div className="flex h-16 w-full justify-around pb-2">
+        <GenreButton
+          name={"Game"}
+          icon={<ControllerIcon className="h-6 w-6"></ControllerIcon>}
+          genre={genre}
+          setGenre={setGenre}
+        ></GenreButton>
+        <GenreButton
+          name={"Film"}
+          icon={<MovieIcon className="h-6 w-6"></MovieIcon>}
+          genre={genre}
+          setGenre={setGenre}
+        ></GenreButton>
+        <GenreButton
+          name={"Book"}
+          icon={<BookIcon strokeWidth={1.5} className="h-6 w-6"></BookIcon>}
+          genre={genre}
+          setGenre={setGenre}
+        ></GenreButton>
+        <GenreButton
+          name={"Audio"}
+          icon={<MusicIcon className="h-6 w-6"></MusicIcon>}
+          genre={genre}
+          setGenre={setGenre}
+        ></GenreButton>
+        <GenreButton
+          name={"Other"}
+          icon={<ControllerIcon className="h-6 w-6"></ControllerIcon>}
+          genre={genre}
+          setGenre={setGenre}
+        ></GenreButton>
+      </div>
       <div className="relative h-auto">
         <input
-          className="mt-1 mb-2 w-full rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition  duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
+          className="mt-1 mb-2 w-full rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
           name="search"
           data-cy="input-entry-media"
           placeholder={placeholder}
@@ -57,13 +98,13 @@ const SearchAPI = ({
         {showGames ? (
           <div
             data-cy="media-dropdown"
-            className="absolute top-11 left-0 z-50 h-auto w-full divide-y-2 rounded-md rounded-t-none border-b-4 border-teal-700 bg-teal-50 py-1 font-medium text-slate-500 shadow-2xl transition duration-300 ease-linear hover:cursor-pointer"
+            className="absolute top-11 left-0 z-50 h-auto w-full divide-y-2 rounded-md rounded-t-none border-b-4 border-teal-700 bg-teal-50 py-1 font-medium text-slate-500 shadow-2xl transition duration-300 ease-linear"
           >
             {games.map((g) => (
               <div
                 key={g.game_id}
                 onMouseDown={(e) => onClickGame(e, g)}
-                className="w-full bg-transparent px-2 py-1 font-normal hover:bg-slate-200"
+                className="w-full bg-transparent px-2 py-1 font-normal hover:cursor-pointer hover:bg-slate-200"
               >
                 {g.title}
               </div>
@@ -72,11 +113,11 @@ const SearchAPI = ({
         ) : (
           <></>
         )}
+        <SearchIcon
+          strokeWidth={1.5}
+          className="absolute right-3 top-4 h-6 w-6 text-teal-800"
+        ></SearchIcon>
       </div>
-      <SearchIcon
-        strokeWidth={1.5}
-        className="absolute right-3 top-4 h-6 w-6 text-teal-800"
-      ></SearchIcon>
     </div>
   );
 };
