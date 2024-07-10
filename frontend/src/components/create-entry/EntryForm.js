3239d04c9d244803c51de9d2fe1e48837994be14
John Cuviello <jcuviell@uci.edu>
2024-07-09 18:02:52 -0700

    fixed responsiveness for create/edit entry form

--- frontend/src/components/create-entry/EntryForm.js
+++ frontend/src/components/create-entry/EntryForm.js
@@ -180,7 +180,7 @@ const EntryForm = () => {
   };
 
   return (
-    <div className="absolute left-0 top-0 z-20 mx-auto flex h-screen w-full max-w-4xl flex-col justify-between rounded-2xl border-b-8 border-slate-600 bg-teal-50 md:static md:my-8 md:h-auto lg:static ">
+    <div className="relative z-20 mx-auto flex h-auto max-h-screen w-full max-w-4xl flex-col justify-between rounded-2xl border-b-8 border-slate-600 bg-teal-50 md:static md:my-8 md:h-auto lg:static">
       <div className="flex w-full items-center justify-between px-4 py-6">
         <h1 className=" mx-4">{currJournal.journalName}</h1>
         <button
@@ -192,88 +192,90 @@ const EntryForm = () => {
           <XIcon className="h-8 w-8 text-teal-900"></XIcon>
         </button>
       </div>
-      <div className=" mx-auto flex w-full flex-col justify-start overflow-y-auto text-teal-900">
-        <div className="flex w-full items-center px-4 text-gray-500 focus-within:text-gray-700  ">
-          <input
-            className="mt-2 mb-2 h-14 w-full rounded-lg bg-transparent p-3 text-3xl font-semibold decoration-teal-700 underline-offset-4 focus:underline focus:outline-none "
-            autoFocus
-            name="title"
-            data-cy="input-entry-title"
-            placeholder="Untitled"
-            autoComplete="off"
-            value={entryTitle}
-            onChange={(e) => setEntryTitle(e.target.value)}
-          ></input>
-        </div>
-
-        <div className="mx-2 flex flex-col px-4 text-left text-lg ">
-          <label className="px-2 text-base font-semibold">Date:</label>
-          <input
-            className="mt-1 mb-2 rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
-            onChange={(e) => {
-              setFormatDate(e.target.value);
-            }}
-            type="datetime-local"
-            name="start-time"
-            data-cy="input-entry-date"
-            value={startDate.slice(0, 16)}
-          ></input>
-          <label className="px-2 text-base font-semibold">Media:</label>
-
-          <div className="relative flex text-teal-900">
-            <SearchAPI
-              data-cy="input-media-component"
-              setMediaObj={setMediaObj}
-              placeholder={"Find a media title..."}
-            ></SearchAPI>
-          </div>
-
-          <label className="px-2 text-base font-semibold">Entry:</label>
-
-          <Textbox
-            initialContent={content}
-            setText={setText}
-            setContent={setContent}
-          ></Textbox>
-
-          <label className="mb-1 px-2 text-base font-semibold ">Tags:</label>
-          <div className="flex items-center justify-between gap-4">
+      <div className="h-full overflow-y-scroll pb-64 sm:p-8">
+        <div className=" mx-auto flex w-full flex-col justify-start overflow-y-auto text-teal-900">
+          <div className="flex w-full items-center px-4 text-gray-500 focus-within:text-gray-700  ">
             <input
-              className="w-full rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
-              type="text"
-              name="curr-tag"
-              data-cy="input-entry-tag"
-              placeholder="Add a tag..."
-              value={currTag}
-              onChange={(e) => setCurrTag(e.target.value)}
-              onKeyUp={handleEnter}
+              className="mt-2 mb-2 h-14 w-full rounded-lg bg-transparent p-3 text-3xl font-semibold decoration-teal-700 underline-offset-4 focus:underline focus:outline-none "
+              autoFocus
+              name="title"
+              data-cy="input-entry-title"
+              placeholder="Untitled"
               autoComplete="off"
+              value={entryTitle}
+              onChange={(e) => setEntryTitle(e.target.value)}
             ></input>
-            <button
-              data-cy="add-tag-button"
-              className="flex shrink-0 items-center justify-center rounded-lg border-b-2 border-solid border-teal-900 bg-teal-600 p-2 text-teal-50 shadow-xl hover:bg-teal-700 active:bg-teal-900 active:shadow-md"
-              onClick={addTag}
-            >
-              <PlusIcon></PlusIcon>
-              <span className="hidden px-2 text-base md:block"> Add Tag</span>
-            </button>
           </div>
 
-          <div
-            data-cy="tags-div"
-            className="mx-auto mt-3 flex h-12 w-full justify-start overflow-x-auto transition duration-300 ease-in-out"
-          >
-            {tags ? (
-              tags.map((t, i) => (
-                <Tag title={t} removeTag={removeTag} key={i}></Tag>
-              ))
-            ) : (
-              <></>
-            )}
+          <div className="mx-2 flex flex-col px-4 text-left text-lg ">
+            <label className="px-2 text-base font-semibold">Date:</label>
+            <input
+              className="mt-1 mb-2 rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
+              onChange={(e) => {
+                setFormatDate(e.target.value);
+              }}
+              type="datetime-local"
+              name="start-time"
+              data-cy="input-entry-date"
+              value={startDate.slice(0, 16)}
+            ></input>
+            <label className="px-2 text-base font-semibold">Media:</label>
+
+            <div className="relative flex text-teal-900">
+              <SearchAPI
+                data-cy="input-media-component"
+                setMediaObj={setMediaObj}
+                placeholder={"Find a media title..."}
+              ></SearchAPI>
+            </div>
+
+            <label className="px-2 text-base font-semibold">Entry:</label>
+
+            <Textbox
+              initialContent={content}
+              setText={setText}
+              setContent={setContent}
+            ></Textbox>
+
+            <label className="mb-1 px-2 text-base font-semibold ">Tags:</label>
+            <div className="flex items-center justify-between gap-4">
+              <input
+                className="w-full rounded-lg bg-slate-300 p-3 shadow-inner shadow-slate-400 outline-8 transition duration-300 ease-in-out focus:bg-teal-50 focus:shadow-none focus:outline-offset-1 focus:outline-teal-700"
+                type="text"
+                name="curr-tag"
+                data-cy="input-entry-tag"
+                placeholder="Add a tag..."
+                value={currTag}
+                onChange={(e) => setCurrTag(e.target.value)}
+                onKeyUp={handleEnter}
+                autoComplete="off"
+              ></input>
+              <button
+                data-cy="add-tag-button"
+                className="flex shrink-0 items-center justify-center rounded-lg border-b-2 border-solid border-teal-900 bg-teal-600 p-2 text-teal-50 shadow-xl hover:bg-teal-700 active:bg-teal-900 active:shadow-md"
+                onClick={addTag}
+              >
+                <PlusIcon></PlusIcon>
+                <span className="hidden px-2 text-base md:block"> Add Tag</span>
+              </button>
+            </div>
+
+            <div
+              data-cy="tags-div"
+              className="mx-auto mt-3 flex h-12 w-full justify-start overflow-x-auto transition duration-300 ease-in-out"
+            >
+              {tags ? (
+                tags.map((t, i) => (
+                  <Tag title={t} removeTag={removeTag} key={i}></Tag>
+                ))
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </div>
       </div>
-      <div className=" mx-auto mb-16 flex w-full max-w-4xl justify-center gap-8 p-4 md:mb-8">
+      <div className="fixed bottom-0 mx-auto flex w-full max-w-4xl justify-center gap-8 bg-teal-50 p-4 pt-8 pb-16 md:static ">
         <button
           id="create-entry-button"
           className="focus flex w-60 items-center justify-center rounded-lg border-2 border-b-2 border-solid border-teal-900 p-2 text-lg font-semibold text-teal-900 shadow-xl hover:bg-slate-300 active:bg-slate-400 active:shadow-md"
